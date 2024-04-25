0990d2d7642d33326c624f443c30b1e7f61dfb03
vandervennetart <art.van.der.vennet.be@gmail.com>
2024-04-25 16:59:16 +0200

    changed directories

---
+++ js/theorie5/oef1.js
@@ -0,0 +1,21 @@
+"use strict";
+(function(){
+
+    const doSomething = function(ms, callback){
+        setTimeout( ()=>{
+            const result = Math.random() > .5
+            if (result) callback(undefined, "hoera")
+            else callback("oeps", undefined)
+        }, ms)
+    }
+
+
+
+    doSomething(2000, (error, result) => {
+        if (error){
+            console.log("err")
+        }else{
+            console.log(result)
+        }
+    });
+})()
---
+++ js/theorie5/oef2.js
@@ -0,0 +1,42 @@
+let chance = .1
+
+const fixCar = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // 50% chance dad fixes the car
+            if (Math.random() > .5) resolve("car fixed");
+            else reject("dad didn't find the problem");
+        }, 1000);
+    });
+};
+
+const goToPark = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // 50% chance of bad weather
+            if (Math.random() > 0.5) resolve("the sun is out");
+            else reject("too much rain");
+        }, 1000);
+    });
+};
+
+const haveIceCream = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // 10% chance of no money
+            if (Math.random() > chance) resolve("yay, ice cream!");
+            else reject("can't pay");
+        }, 1000);
+    });
+};
+
+fixCar()
+    .catch(() => {
+        console.log("taking the bus");
+        chance = .5
+    })
+    .then(goToPark)
+    .then(haveIceCream)
+    .then(() => console.log("good day"))
+    .catch((err) => console.log(err))
+    .finally(() => console.log("bye bye now"));
